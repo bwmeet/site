e953b4aa17c57361fe01eb33a375e064d67aca76
Tim Stone <tim@fetimo.com>
2014-11-26 21:14:31 +0000

    Converted to PHP for easier updating

--- about.php
+++ about.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html>
-    <head>
-        <title>B &amp; W Meet</title>
-        <link href='http://fonts.googleapis.com/css?family=Fira+Sans:400,700,400italic,300' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="/css/bwmeet-embedded.css" media="screen">
-        <link rel="stylesheet" href="/css/normalize.min.css" media="screen">
-        <link rel="stylesheet" href="/css/master.css" media="screen">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <meta charset="utf-8">
-    </head>
-    <body>
+<?php require_once 'header.php'; ?>
         <header id="top">
             <img src="images/logo.png" alt="The B &amp; W Meet logo" class="logo circle" width="180" height="180">
             <p class="strapline">
@@ -17,19 +6,19 @@
                 for creatives, developers &amp; tech enthusiasts
             </p>
             <div class="information">
-                <p class="left"><a href="event.ics" download><i class="icon-clock"></i>19<sup>th</sup> September 2014</a></p>
-                <p class="middle"><a href="https://www.google.co.uk/maps/place/Bermuda+Triangle/@50.723962,-1.953475,17z/data=!3m1!4b1!4m2!3m1!1s0x4873a135131b9d3f:0xcc4fcd31e89a6c5"><i class="icon-location"></i>Bermuda Triangle</a></p>
+                <p class="left"><a href="event.ics" download><i class="icon-clock"></i><?php echo $eventDay; ?><sup><?php echo $eventSuffix; ?></sup> <?php echo $eventMonth; echo ' '; echo $eventYear; ?></a></p>
+                <p class="middle"><a href="<?php echo $eventLocationMapLink; ?>"><i class="icon-location"></i><?php echo $eventLocation; ?></a></p>
                 <p class="right"><i class="icon-pound"></i>Free</p>
             </div>
-            <a href="http://bwmeet15.eventbrite.co.uk" class="cta-primary">Get tickets</a>
+            <a href="<?php echo $eventUrl; ?>" class="cta-primary">Get tickets</a>
             <!-- <a href="#" class="cta-secondary">Find out more</a> -->
         </header>
         <nav id="site-nav">
             <ul class="constrained">
                 <li><a href="#top" class="exempt"><i class="icon-home"></i></a></li>
                 <li><a href="index.html">About <span class="hide-mobile">B &amp; W Meet</span></a></li>
-                <li><a href="about.html">Who<span class="hide-mobile"> are we</span>?</a></li>
-                <li><a class="active" href="sponsors.html">Sponsor</a></li>
+                <li><a class="active" href="about.html">Who<span class="hide-mobile"> are we</span>?</a></li>
+                <li><a href="sponsors.html">Sponsor</a></li>
             </ul>
         </nav>
         <article class="constrained center index is-hidden">
@@ -40,7 +29,7 @@
             <p>It doesn’t matter where you’re from or how experienced you are as long as you’re passionate about the web.</p>
             <p>We’ll always be free and open to all. We look forward to meeting you &ndash; or seeing you again!</p>
         </article>
-        <article class="constrained center about is-hidden">
+        <article class="constrained center about">
             <h1>Who we are</h1>
             <p>We love building for the web and we love meeting people&mdash;that's why B &amp; W Meet is a thing.</p>
             <div class="bio media">
@@ -70,7 +59,7 @@
                 </p>
             </section>
         </article>
-        <article class="constrained center sponsors">
+        <article class="constrained center sponsors is-hidden">
             <h1>Our sponsors</h1>
             <p>We wouldn't be able to run a great event without our wonderful sponsor.</p>
             <div class="sponsor">
@@ -78,26 +67,4 @@
                 <p><a href="https://www.campaignmonitor.com/">Campaign Monitor</a> makes it easy to attract new subscribers, send them beautiful email newsletters and see stunning reports on the results. <em>They're also tremendously nice people.</em></p>
             </div>
         </article>
-        <div class="join-us cf">
-            <div class="constrained">
-                <div>
-                    <h3>Interested in joining us?</h3>
-                    <p>Come and get your free ticket and get involved with the local community</p>
-                </div>
-                <a href="http://bwmeet15.eventbrite.co.uk" class="cta-tertiary">Get tickets</a>
-            </div>
-        </div>
-        <footer>
-            <p class="constrained">Site design by <a href="http://killercreations.co.uk/">KillerCreations</a>. Made with ❤ in Dorset.
-        </footer>
-        <script src="navigation.js"></script>
-        <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-11418004-2', 'auto');
-      ga('send', 'pageview');
-    </script>
-    </body>
-</html>
+<?php require_once 'footer.php'; ?>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>B &amp; W Meet</title>
+        <link href='http://fonts.googleapis.com/css?family=Fira+Sans:400,700,400italic,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="/css/bwmeet-embedded.css" media="screen">
+        <link rel="stylesheet" href="/css/normalize.min.css" media="screen">
+        <link rel="stylesheet" href="/css/master.css" media="screen">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta charset="utf-8">
+    </head>
+    <body>
