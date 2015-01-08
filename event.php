23c20fedadc00c4fb07a6f09de72276d89ea7328
Tim Stone <tim@fetimo.com>
2015-01-08 22:39:07 +0000

    Updated date suffix

--- event.php
+++ event.php
@@ -6,7 +6,7 @@ $eventLocation = 'Inferno';
 $eventLocationMapLink = 'https://www.google.co.uk/maps/place/Inferno/@50.723218,-1.864838,17z/data=!4m6!1m3!3m2!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3!2sInferno!3m1!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3';
 $eventDate = '20/01/2014';
 $eventDay = 20;
-$eventSuffix = 'st';
+$eventSuffix = 'th';
 $eventMonth = 'January';
 $eventYear = 2015;
 
