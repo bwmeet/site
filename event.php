7e2c2d39cb03a4de24afb6d65b36dcc375d2262f
Tim Stone <tim@fetimo.com>
2015-01-08 19:19:42 +0000

    Updated event details

--- event.php
+++ event.php
@@ -1,13 +1,13 @@
 <?php
 
-$eventName = 'B & W Meet 16';
-$eventUrl = 'http://bwmeet16.eventbrite.co.uk/';
-$eventLocation = 'Bermuda Triangle';
-$eventLocationMapLink = 'https://www.google.co.uk/maps/place/Bermuda+Triangle/@50.723962,-1.953475,17z/data=!3m1!4b1!4m2!3m1!1s0x4873a135131b9d3f:0xcc4fcd31e89a6c5';
-$eventDate = '16/12/2014';
-$eventDay = 16;
-$eventSuffix = 'th';
-$eventMonth = 'December';
-$eventYear = 2014;
+$eventName = 'B & W Meet 17';
+$eventUrl = 'http://bwmeet17.eventbrite.co.uk/';
+$eventLocation = 'Inferno';
+$eventLocationMapLink = 'https://www.google.co.uk/maps/place/Inferno/@50.723218,-1.864838,17z/data=!4m6!1m3!3m2!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3!2sInferno!3m1!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3';
+$eventDate = '20/01/2014';
+$eventDay = 20;
+$eventSuffix = 'st';
+$eventMonth = 'January';
+$eventYear = 2015;
 
 ?>
