adb3dbd117dd8ad58411520118b28563a7669ddd
Tim Stone <tim@fetimo.com>
2015-11-17 19:42:06 +0000

    Updated event data

--- event.php
+++ event.php
@@ -1,13 +1,13 @@
 <?php
 
-$eventName = 'B & W Meet 18';
-$eventUrl = 'https://bwmeet18.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
+$eventName = 'B & W Meet 19';
+$eventUrl = 'https://bwmeet19.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
 $eventLocation = 'The Slug and Lettuce';
 $eventLocationMapLink = 'https://www.google.co.uk/maps/place/The+Slug+And+Lettuce/@50.7206979,-1.8794283,15z/data=!4m2!3m1!1s0x0:0x1b4f21a18ece51e7?sa=X&ved=0CHgQ_BIwDWoVChMIoarBx7P7xgIVQzwUCh2q9Q58';
-$eventDate = '01/09/2015'; //20/01/2015
-$eventDay = '1'; //20
-$eventSuffix = 'st'; //th
-$eventMonth = 'September'; // January
+$eventDate = '09/23/2015'; //20/01/2015
+$eventDay = '9'; //20
+$eventSuffix = 'th'; //th
+$eventMonth = 'December'; // January
 $eventYear = '2015'; //2015
 
 ?>
