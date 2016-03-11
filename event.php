c392196e3f5947db42afd6e1029e58886c07eb05
Tim Stone <tim@fetimo.com>
2016-03-11 12:29:36 +0000

    Updated event details.

--- event.php
+++ event.php
@@ -2,17 +2,19 @@
 
 date_default_timezone_set('Europe/London');
 
-$eventName = 'B & W Meet 20';
-$eventUrl = 'https://bwmeet20.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
-$eventLocation = 'The Slug and Lettuce';
-$eventLocationMapLink = 'https://www.google.co.uk/maps/place/The+Slug+And+Lettuce/@50.7206979,-1.8794283,15z/data=!4m2!3m1!1s0x0:0x1b4f21a18ece51e7?sa=X&ved=0CHgQ_BIwDWoVChMIoarBx7P7xgIVQzwUCh2q9Q58';
-$eventDate = '16/02/2016'; //20/01/2015
-$eventDay = '16'; //20
+$eventName = 'B & W Meet 21';
+$eventUrl = 'https://bwmeet21.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
+$eventLocation = 'TBC'; //The Slug and Lettuce
+// $eventLocationMapLink = 'https://www.google.co.uk/maps/place/The+Slug+And+Lettuce/@50.7206979,-1.8794283,15z/data=!4m2!3m1!1s0x0:0x1b4f21a18ece51e7?sa=X&ved=0CHgQ_BIwDWoVChMIoarBx7P7xgIVQzwUCh2q9Q58';
+$eventDate = '12/04/2016'; //20/01/2015
+$eventDay = '12'; //20
 $eventSuffix = 'th'; //th
-$eventMonth = 'February'; // January
+$eventMonth = 'April'; // January
 $eventYear = '2016'; //2015
-$eventIsInPast = false;
 
+
+
+$eventIsInPast = false;
 
 // work out if event date is in past
 $date = strptime($eventDate, '%d/%m/%Y');
