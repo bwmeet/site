619a0bb479ae8be4abab06e748fec626aec0dfed
Tim Stone <tim@fetimo.com>
2016-05-01 13:25:37 +0100

    Updated event details

--- event.php
+++ event.php
@@ -2,15 +2,15 @@
 
 date_default_timezone_set('Europe/London');
 
-$eventName = 'B & W Meet 21';
-$eventUrl = 'https://bwmeet21.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
-$eventLocation = 'The Inferno'; //The Slug and Lettuce
-// $eventLocationMapLink = 'https://www.google.co.uk/maps/place/The+Slug+And+Lettuce/@50.7206979,-1.8794283,15z/data=!4m2!3m1!1s0x0:0x1b4f21a18ece51e7?sa=X&ved=0CHgQ_BIwDWoVChMIoarBx7P7xgIVQzwUCh2q9Q58';
-$eventLocationMapLink = 'https://www.google.co.uk/maps/place/Inferno/@50.7232011,-1.867011,17z/data=!4m7!1m4!3m3!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3!2sInferno!3b1!3m1!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3';
-$eventDate = '12/04/2016'; //20/01/2015
-$eventDay = '12'; //20
-$eventSuffix = 'th'; //th
-$eventMonth = 'April'; // January
+$eventName = 'B & W Meet 22';
+$eventUrl = 'https://bwmeet22.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
+$eventLocation = 'The Slug and Lettuce'; //The Slug and Lettuce
+$eventLocationMapLink = 'https://www.google.co.uk/maps/place/The+Slug+And+Lettuce/@50.7206979,-1.8794283,15z/data=!4m2!3m1!1s0x0:0x1b4f21a18ece51e7?sa=X&ved=0CHgQ_BIwDWoVChMIoarBx7P7xgIVQzwUCh2q9Q58';
+// $eventLocationMapLink = 'https://www.google.co.uk/maps/place/Inferno/@50.7232011,-1.867011,17z/data=!4m7!1m4!3m3!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3!2sInferno!3b1!3m1!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3';
+$eventDate = '31/05/2016'; //20/01/2015
+$eventDay = '31'; //20
+$eventSuffix = 'st'; //th
+$eventMonth = 'May'; // January
 $eventYear = '2016'; //2015
 
 
