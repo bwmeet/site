4a82c99d3b4b4a4fb986476aa8d8172aa7396234
Tim Stone <tim@fetimo.com>
2016-11-16 21:31:03 +0000

    Updated EB url

--- event.php
+++ event.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/London');
 
 $eventName = 'B & W Meet 25';
-$eventUrl = 'https://bwmeet25.eventbrite.co.uk/'; //http://bwmeet17.eventbrite.co.uk/
+$eventUrl = 'https://www.eventbrite.co.uk/e/b-w-meet-25-tickets-29411510620'; //http://bwmeet17.eventbrite.co.uk/
 $eventLocation = 'Inferno'; //The Slug and Lettuce
 // $eventLocationMapLink = 'https://www.google.co.uk/maps/place/The+Slug+And+Lettuce/@50.7206979,-1.8794283,15z/data=!4m2!3m1!1s0x0:0x1b4f21a18ece51e7?sa=X&ved=0CHgQ_BIwDWoVChMIoarBx7P7xgIVQzwUCh2q9Q58';
 $eventLocationMapLink = 'https://www.google.co.uk/maps/place/Inferno/@50.7232011,-1.867011,17z/data=!4m7!1m4!3m3!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3!2sInferno!3b1!3m1!1s0x4873a1deeb57a4d3:0xc031e826357a7bb3';
